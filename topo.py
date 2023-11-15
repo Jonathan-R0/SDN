a8ba43882ba52d14bda8a4e87d9c6bb81331ab87
NicoPodesta <you@example.com>
2023-11-15 19:40:01 -0300

    Refactor topology to use descriptive names for switches and hosts

--- topo.py
+++ topo.py
@@ -1,14 +1,13 @@
 from mininet.topo import Topo
-import sys
 
 class Topo(Topo):
-    def build(self, num_switches=2):
-        switches = [self.addSwitch(f's{i}') for i in range(1, num_switches + 1)]
+    def __init__(self, num_switches=2):
+        switches = [self.addSwitch(f'switch_{i}') for i in range(1, num_switches + 1)]
         for i in range(num_switches - 1):
             self.addLink(switches[i], switches[i + 1])
-        host1 = self.addHost('h1')
+        host1 = self.addHost('host_1')
         self.addLink(switches[0], host1)
-        host2 = self.addHost('h2')
+        host2 = self.addHost('host_2')
         self.addLink(switches[-1], host2)
 
 topos = { 'customTopo': Topo }
