86ecbd2d805b5af6b71eef1f237bf7addec6e38b
Nicolas Podesta <npodesta@fi.uba.ar>
2023-11-15 22:35:31 -0300

    Updated customTopo in topo.py and command in README.md for better functionality

--- topo.py
+++ topo.py
@@ -1,7 +1,8 @@
 from mininet.topo import Topo
 
-class Topo(Topo):
+class CustomTopo(Topo):
     def __init__(self, num_switches=2):
+        Topo.__init__(self)
         switches = [self.addSwitch(f'switch_{i}') for i in range(1, num_switches + 1)]
         for i in range(num_switches - 1):
             self.addLink(switches[i], switches[i + 1])
@@ -14,4 +15,4 @@ class Topo(Topo):
         host4 = self.addHost('host_4')
         self.addLink(switches[-1], host4)
 
-topos = { 'customTopo': Topo }
+topos = { 'customTopo': CustomTopo }
