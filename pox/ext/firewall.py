79881f99e1eefbb9ef2583acbdf5c53aa1bb6ee6
Nicolas Podesta <npodesta@fi.uba.ar>
2023-11-19 18:49:59 -0300

    Refactored firewall module and added packet handling

--- pox/ext/firewall.py
+++ pox/ext/firewall.py
@@ -1,9 +1,3 @@
-# coding: utf-8
-# Coursera :
-# - Software Defined Networking ( SDN ) course
-# -- Programming Assignment : Layer -2 Firewall Application Professor : Nick Feamster
-# Teaching Assistant : Arpit Gupta
-
 from nis import match
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
@@ -21,31 +15,23 @@ class Firewall(EventMixin):
 
     def __init__(self):
         self.listenTo(core.openflow)
-        '''
-        self.h1 = EthAddr("00:00:00:00:00:01")
-        self.h2 = EthAddr("00:00:00:00:00:02")
-        '''
         self.rules = self._import_rules()
         log.debug("Enabling Firewall Module")
 
     def _import_rules(self):
         with open("./ext/rules.rules", "r") as f:
             return json.load(f)
+        
+    def _handle_PacketIn(self, event):
+        packet = event.parsed
+        log.debug("Received packet: %s", packet)
 
     def _handle_ConnectionUp(self, event):
         log.debug("Switch %s has come up.", dpidToStr(event.dpid))
         # Agregar reglas al switch cuando se establece la conexión
         for rule in self.rules:
             self.add_rule(rule, event)
-        '''
-        if self.rules["rule_1"]:
-            self.filter_port_80(event)
-        elif self.rules["rule_2"]:
-            self.filter_UDP_h1_5001(event)
-        elif self.rules["rule_3"]:
-            self.filter_hosts(event)
-            '''
-    
+
     def add_rule(self, rule, event):
         match = of.ofp_match()
         match.dl_type = pkt.ethernet.IP_TYPE
@@ -61,37 +47,10 @@ class Firewall(EventMixin):
             elif rule["nw_proto"] == "TCP":
                 match.nw_proto = pkt.ipv4.TCP_PROTOCOL
         msg = of.ofp_flow_mod(match=match)
+        log.debug("Adding rule: %s", str(rule))
+        log.debug("Sending flow mod: %s", str(msg))
         event.connection.send(msg)
         
-    '''
-    def filter_UDP_h1_5001(self, event):
-        # Regla 2: Descartar mensajes que provengan del host 1, tengan como puerto destino el 5001 y usen UDP
-        match = of.ofp_match()
-        match.dl_type = pkt.ethernet.IP_TYPE
-        match.dl_src = EthAddr('00:00:00:00:00:01')
-        match.tp_dst = 5001
-        match.nw_proto = pkt.ipv4.UDP_PROTOCOL
-        self.add_drop_rule(event, match)
-
-    def filter_port_80(self, event):
-        match = of.ofp_match()
-        match.dl_type = pkt.ethernet.IP_TYPE
-        match.nw_proto = pkt.ipv4.TCP_PROTOCOL
-        match.tp_dst = 80
-        self.add_drop_rule(event, match)
-
-    def filter_hosts(self, event):
-        # Regla 3: Bloquear la comunicación entre dos hosts específicos
-        match = of.ofp_match()
-        match.dl_type = pkt.ethernet.IP_TYPE
-        match.nw_src = self.h1
-        match.nw_dst = self.h2
-        self.add_drop_rule(event, match)
-
-    def add_drop_rule(self, event, match):
-        msg = of.ofp_flow_mod(match=match)
-        event.connection.send(msg)
-    '''
 
 def launch():
     '''
